4438c4e06f5b87f77960eb4facb84597c0f72361
Jake Morales <jmora060@ucr.edu>
2020-11-06 01:40:18 -0800

    implements CV link

--- _javascript/main.js
+++ _javascript/main.js
@@ -3,6 +3,12 @@ let tab_contents = document.querySelectorAll('.tab-content');
 
 // Switches tabs
 tabs.forEach((tab, index) => {
+  if(tab.textContent == "CV") {
+    tab.addEventListener('click', () => {
+      window.location.href = "/AlysiaBurbidge_CV.pdf"
+    })
+    return
+  }
   tab.addEventListener('click', () => {
     tabs.forEach((t) => t.classList.remove("is-active"))
     tab_contents.forEach((tc) => tc.classList.add("is-hidden"))
